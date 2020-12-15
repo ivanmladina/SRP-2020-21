1d6a55fb64f97f3d029154c03bade5a95d5acadf
ivanmladina99 <ivanmladina99@gmail.com>
2020-12-15 18:03:10 +0100

    Update user service to store hashed password

--- postgres-express-node/config/index.js
+++ postgres-express-node/config/index.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
+
 const envFound = dotenv.config();
 if (envFound.error) {
   throw new Error("Couldn't find .env file");
@@ -39,5 +40,9 @@ module.exports = {
         },
       ],
     },
+  },
+
+  bcrypt: {
+    SALT_ROUNDS: 12,
   },
 };
